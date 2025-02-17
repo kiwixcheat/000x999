8fc4827b1fbfb36f7e0ab6cb7308536125961dbd
kiwixcheat <162964063+kiwixcheat@users.noreply.github.com>
2025-02-17 16:12:04 -0300

    Update KiwiX Hub X Solara .lua

--- KiwiX Hub X Solara .lua
+++ KiwiX Hub X Solara .lua
@@ -1,1 +1,1 @@
-print("The script will be under maintenance, new features will be added: auto invasion, and the UI library will be changed")
+print("The script will be under maintenance, new features will be added: auto invasion, and the UI library will be changed, Discord: discord.gg/9J5st8UUTh")
